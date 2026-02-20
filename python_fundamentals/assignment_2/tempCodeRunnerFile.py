b7ab45271f951f6acee3051fe9b9a125bf2297a9
Omvishesh <omvishesh123@gmail.com>
2026-02-20 22:42:03 +0530

    deleted temp python file

--- python_fundamentals/assignment_2/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-user = input("enter the number : ")
-
-while user:
-    if user == "Quit":
-        break
-    elif int(user) >= 0:
-        print("positive")
-    elif int(user) < 0:
-        print('negative')
